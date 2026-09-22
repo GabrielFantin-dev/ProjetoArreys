8931a66c91c12e7f17d19fd6bf0e45479e5a1dd4
Gabriel Fantin <fantinee@live.com>
2026-09-22 11:02:20 -0300

    Create ListaDeConvidados.java

---
+++ ListaDeConvidados.java
@@ -0,0 +1,11 @@
+
+public class ListaDeConvidados{
+     String lista[] = new String[3];
+     lista[0] = "Ana";
+     lista[1] = "Bruno";
+     lista[2] = "Fernando";
+
+     for (String convidado : lista){
+         System.out.println(convidado);
+     }
+}   
